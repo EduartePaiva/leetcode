28f1ff4cfbb36bb48ee5a78ca06e6645f4fe0594
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-03-30 15:02:44 -0300

    992. Subarrays with K Different Integers

--- rust/src/daily/leetcode0992.rs
+++ rust/src/daily/leetcode0992.rs
@@ -6,37 +6,30 @@ impl Solution {
     pub fn subarrays_with_k_distinct(nums: Vec<i32>, k: i32) -> i32 {
         let k = k as usize;
         let mut count = HashMap::new();
-        let mut prefix_count = vec![0; nums.len()];
-        let mut prefix_un_count = vec![0; nums.len()];
 
-        for i in 0..nums.len() {
-            *count.entry(nums[i]).or_insert(0) += 1;
-            prefix_count[i] = count.len();
-        }
-        let max_cnt = count.len();
-        for i in 0..nums.len() {
-            *count.entry(nums[i]).or_insert(0) -= 1;
-            prefix_un_count[i] = max_cnt - count.len();
-            if count.get(&nums[i]).unwrap() == &0 {
-                count.remove(&nums[i]);
-            }
-        }
-        println!("{:?}", nums);
-        println!("{:?}", prefix_count);
-        println!("{:?}", prefix_un_count);
-
-        let mut l = 0;
         let mut res = 0;
+        let mut l_near = 0;
+        let mut l_far = 0;
         for r in 0..nums.len() {
-            if prefix_count[r] - prefix_un_count[l] == k {
-                res += 1;
-                while prefix_count[r] - prefix_un_count[l] == k {
-                    res += 1;
-                    l += 1;
+            *count.entry(nums[r]).or_insert(0) += 1;
+
+            while count.len() > k {
+                *count.entry(nums[l_near]).or_insert(0) -= 1;
+                if count.get(&nums[l_near]).unwrap() == &0 {
+                    count.remove(&nums[l_near]);
                 }
+                l_near += 1;
+                l_far = l_near;
             }
 
-            println!("r: {r}, l: {l}");
+            if count.get(&nums[l_near]).unwrap() > &1 {
+                *count.entry(nums[l_near]).or_insert(0) -= 1;
+                l_near += 1;
+            }
+
+            if count.len() == k {
+                res += l_near - l_far + 1;
+            }
         }
 
         res as i32
