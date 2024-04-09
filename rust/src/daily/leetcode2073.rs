629e1623c74c42bd100c38d55c81a91adbd4b368
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-09 11:20:06 -0300

    better solution than mine

--- rust/src/daily/leetcode2073.rs
+++ rust/src/daily/leetcode2073.rs
@@ -1,36 +1,19 @@
 // 2073. Time Needed to Buy Tickets
 pub struct Solution;
 
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 impl Solution {
     pub fn time_required_to_buy(tickets: Vec<i32>, k: i32) -> i32 {
-        let k = k as usize;
-        let mut tickets_heap = BinaryHeap::with_capacity(tickets.len());
-        for (i, val) in tickets.into_iter().enumerate() {
-            tickets_heap.push((Reverse(val), i))
+        let mut res = 0;
+        let k_val = tickets[k as usize];
+
+        for i in 0..=(k as usize) {
+            res += tickets[i].min(k_val);
+        }
+        for i in ((k + 1) as usize)..tickets.len() {
+            res += tickets[i].min(k_val - 1);
         }
 
-        let mut cur_time = 0;
-        let mut prev_val = 0;
-        let mut i_shift = 0;
-
-        while !tickets_heap.is_empty() {
-            let start_heap_length = tickets_heap.len() as i32;
-            let top_val = tickets_heap.peek().unwrap().0 .0;
-            cur_time += (top_val - prev_val) * start_heap_length;
-            prev_val = top_val;
-            while !tickets_heap.is_empty() && tickets_heap.peek().unwrap().0 .0 == top_val {
-                if let Some((_, i)) = tickets_heap.pop() {
-                    if i < k {
-                        i_shift += 1;
-                    } else if i == k {
-                        return cur_time - (start_heap_length - 1 - (i - i_shift) as i32);
-                    }
-                }
-            }
-        }
-        cur_time
+        res
     }
 }
 #[cfg(test)]
