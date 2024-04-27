1929b5148c340198a59cf7ee426bed2e6b4acf94
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-27 11:13:57 -0300

    recursive solution

--- rust/src/daily/leetcode0514.rs
+++ rust/src/daily/leetcode0514.rs
@@ -3,6 +3,7 @@
 // 514. Freedom Trail
 pub struct Solution;
 
+use std::collections::HashMap;
 impl Solution {
     pub fn find_rotate_steps(ring: String, key: String) -> i32 {
         let ring = ring.as_bytes();
@@ -18,58 +19,37 @@ impl Solution {
             .map(|k| (k - b'a') as usize)
             .collect();
 
-        fn greedy_dist(my_pos: usize, chars: &Vec<usize>, ring_size: usize) -> (i32, usize) {
-            let mut steps = i32::MAX;
-            let mut new_pos = 0;
-
+        let mut cache: HashMap<(usize, usize), i32> = HashMap::new();
+        fn dfs(
+            ring_i: usize,
+            key_i: usize,
+            char_map: &[Vec<usize>; 26],
+            key: &Vec<usize>,
+            ring_size: usize,
+            cache: &mut HashMap<(usize, usize), i32>,
+        ) -> i32 {
+            if key.len() == key_i {
+                return 0;
+            }
+            if let Some(val) = cache.get(&(ring_i, key_i)) {
+                return *val;
+            }
+            let mut min_cost = i32::MAX;
+            let chars = &char_map[key[key_i]];
             for c in chars {
-                if c == &my_pos {
-                    return (0, my_pos);
-                }
-                //going front
-                if c > &my_pos {
-                    let n_steps = (c - &my_pos) as i32;
-                    if n_steps < steps {
-                        steps = n_steps;
-                        new_pos = *c;
-                    }
-                } else {
-                    let n_steps = ((ring_size - my_pos) + *c) as i32;
-                    if n_steps < steps {
-                        steps = n_steps;
-                        new_pos = *c;
-                    }
-                }
-                //going back
+                min_cost = min_cost.min(
+                    dfs(*c, key_i + 1, char_map, key, ring_size, cache)
+                        + i32::min(
+                            key_i.abs_diff(*c) as i32,
+                            (ring_size - key_i.abs_diff(*c)) as i32,
+                        ),
+                );
             }
-
-            (steps, new_pos)
+            cache.insert((ring_i, key_i), min_cost);
+            min_cost
         }
 
-        fn dfs(ring_i: usize, key_i: usize, char_map: &[Vec<usize>; 26], key: &Vec<usize>) -> i32 {
-            0
-        }
-
-        let steps = key.len() as i32;
-        for k in key {
-            let chars = &char_map[k];
-
-            for char in chars {
-                // here I know the current position of the char based on the variation on the ring
-                // now I calculate the min move and if it's positive or negative
-                // check for positive move
-            }
-        }
-
-        println!("{:?}", char_map);
-
-        //let mut ring_dec: VecDeque<u8> = ring.as_bytes().into_iter().map(|x| *x).collect();
-
-        // map all characters
-        // find all characters
-        // find closest one
-
-        steps
+        dfs(0, 0, &char_map, &key, ring.len(), &mut cache) + key.len() as i32
     }
 }
 
