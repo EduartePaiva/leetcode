afcc7960c1ff1906122899c3ca05f86dc4d12bab
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-27 12:03:46 -0300

    lc 514 dp solution

--- rust/src/daily/leetcode0514.rs
+++ rust/src/daily/leetcode0514.rs
@@ -3,7 +3,6 @@
 // 514. Freedom Trail
 pub struct Solution;
 
-use std::collections::HashMap;
 impl Solution {
     pub fn find_rotate_steps(ring: String, key: String) -> i32 {
         let ring = ring.as_bytes();
@@ -12,44 +11,28 @@ impl Solution {
             let index = (ring[i] - b'a') as usize;
             char_map[index].push(i);
         }
-
         let key: Vec<usize> = key
             .as_bytes()
             .into_iter()
             .map(|k| (k - b'a') as usize)
             .collect();
 
-        let mut cache: HashMap<(usize, usize), i32> = HashMap::new();
-        fn dfs(
-            ring_i: usize,
-            key_i: usize,
-            char_map: &[Vec<usize>; 26],
-            key: &Vec<usize>,
-            ring_size: usize,
-            cache: &mut HashMap<(usize, usize), i32>,
-        ) -> i32 {
-            if key.len() == key_i {
-                return 0;
+        let mut dp = vec![0; ring.len()];
+        for k in (0..key.len()).rev() {
+            let mut next_dp = vec![0; ring.len()];
+            for r in 0..ring.len() {
+                let mut min_cost = i32::MAX;
+                let chars = &char_map[key[k]];
+                for c in chars {
+                    let min_dist =
+                        i32::min(r.abs_diff(*c) as i32, (ring.len() - r.abs_diff(*c)) as i32);
+                    min_cost = min_cost.min(dp[*c] + min_dist);
+                }
+                next_dp[r] = min_cost;
             }
-            if let Some(val) = cache.get(&(ring_i, key_i)) {
-                return *val;
-            }
-            let mut min_cost = i32::MAX;
-            let chars = &char_map[key[key_i]];
-            for c in chars {
-                min_cost = min_cost.min(
-                    dfs(*c, key_i + 1, char_map, key, ring_size, cache)
-                        + i32::min(
-                            key_i.abs_diff(*c) as i32,
-                            (ring_size - key_i.abs_diff(*c)) as i32,
-                        ),
-                );
-            }
-            cache.insert((ring_i, key_i), min_cost);
-            min_cost
+            dp = next_dp;
         }
-
-        dfs(0, 0, &char_map, &key, ring.len(), &mut cache) + key.len() as i32
+        return dp[0] + key.len() as i32;
     }
 }
 
