2ce02b6ca5b41c6d476bc754a6ae03ca2fb5392d
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-28 10:37:29 -0300

    834. Sum of Distances in Tree, I could not solve this neither understand it

--- rust/src/daily/leetcode0834.rs
+++ rust/src/daily/leetcode0834.rs
@@ -5,35 +5,52 @@ impl Solution {
     pub fn sum_of_distances_in_tree(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
         //build the graph. nodes is from 0 to n-1
         let mut graph: Vec<Vec<usize>> = vec![vec![]; n as usize];
+        let mut count: Vec<i32> = vec![0; n as usize];
+        let mut res: Vec<i32> = vec![0; n as usize];
         for edge in edges {
             let n1 = edge[0] as usize;
             let n2 = edge[1] as usize;
             graph[n1].push(n2);
             graph[n2].push(n1);
         }
-        //brute force dfs
-        fn dfs(graph: &Vec<Vec<usize>>, n: usize, prev: usize, depth: i32) -> i32 {
-            if graph[n].len() == 1 && graph[n][0] == prev {
-                // this is a leave
-                return 0;
-            }
 
-            let next_nodes = &graph[n];
-            let mut acc = 0;
-            for node in next_nodes {
-                if *node == prev {
-                    continue;
+        fn dfs1(
+            cur: usize,
+            parent: usize,
+            graph: &Vec<Vec<usize>>,
+            count: &mut Vec<i32>,
+            res: &mut Vec<i32>,
+        ) {
+            count[cur] = 1;
+            let children = &graph[cur];
+            for &child in children {
+                if child != parent {
+                    dfs1(child, cur, graph, count, res);
+                    count[cur] += count[child];
+                    res[cur] += res[child] + count[child];
                 }
-                acc += depth + dfs(graph, *node, n, depth + 1);
             }
-
-            acc
         }
 
-        let mut res = vec![0; n as usize];
-        for i in 0..n as usize {
-            res[i] = dfs(&graph, i, n as usize, 1);
+        fn dfs2(
+            cur: usize,
+            parent: usize,
+            n: usize,
+            count: &Vec<i32>,
+            res: &mut Vec<i32>,
+            graph: &Vec<Vec<usize>>,
+        ) {
+            let children = &graph[cur];
+            for &child in children {
+                if child != parent {
+                    res[child] = res[cur] + n as i32 - 2 * count[child];
+                    dfs2(child, cur, n, count, res, graph);
+                }
+            }
         }
+
+        dfs1(0, n as usize, &graph, &mut count, &mut res);
+        dfs2(0, n as usize, n as usize, &count, &mut res, &graph);
 
         res
     }
