96d6d8b8f886f8fa84d61b5a54a575a04cbf9b2f
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-04 08:54:59 -0300

    1508. Range Sum of Sorted Subarray Sums

--- rust/src/daily/leetcode1508.rs
+++ rust/src/daily/leetcode1508.rs
@@ -2,10 +2,25 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn range_sum(nums: Vec<i32>, n: i32, left: i32, right: i32) -> i32 {
+    pub fn range_sum(nums: Vec<i32>, _n: i32, left: i32, right: i32) -> i32 {
         let mut new_array = vec![];
 
-        for i in 
+        for i in 0..nums.len() {
+            let mut acc = 0;
+            for j in i..nums.len() {
+                acc += nums[j];
+                new_array.push(acc);
+            }
+        }
+        new_array.sort_unstable();
+        const MODULE: i32 = 10_i32.pow(9) + 7;
+        let mut res = 0;
+        for i in (left - 1) as usize..right as usize {
+            res += new_array[i];
+            res %= MODULE;
+        }
+
+        res
     }
 }
 
