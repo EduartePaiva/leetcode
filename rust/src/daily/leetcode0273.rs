3f367c2b78a4dfa52cb1acdb1fa9db2cfdf0071b
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-07 12:22:48 -0300

    273. Integer to English Words

---
+++ rust/src/daily/leetcode0273.rs
@@ -0,0 +1,147 @@
+// 273. Integer to English Words
+pub struct Solution;
+
+use std::collections::HashMap;
+impl Solution {
+    pub fn number_to_words(mut num: i32) -> String {
+        if num == 0 {
+            return "Zero".to_string();
+        }
+
+        let words = HashMap::from([
+            (0, "Zero"),
+            (1, "One"),
+            (2, "Two"),
+            (3, "Three"),
+            (4, "Four"),
+            (5, "Five"),
+            (6, "Six"),
+            (7, "Seven"),
+            (8, "Eight"),
+            (9, "Nine"),
+            (10, "Ten"),
+            (11, "Eleven"),
+            (12, "Twelve"),
+            (13, "Thirteen"),
+            (14, "Fourteen"),
+            (15, "Fifteen"),
+            (16, "Sixteen"),
+            (17, "Seventeen"),
+            (18, "Eighteen"),
+            (19, "Nineteen"),
+            (20, "Twenty"),
+            (30, "Thirty"),
+            (40, "Forty"),
+            (50, "Fifty"),
+            (60, "Sixty"),
+            (70, "Seventy"),
+            (80, "Eighty"),
+            (90, "Ninety"),
+            (100, "Hundred"),
+            (1000, "Thousand"),
+            (1000000, "Million"),
+        ]);
+
+        fn get_hundred(number: i32, words: &HashMap<i32, &str>) -> String {
+            match number {
+                0 => String::from(""),
+                1..=19 => String::from(*words.get(&number).unwrap()),
+                20 => String::from(*words.get(&number).unwrap()),
+                30 => String::from(*words.get(&number).unwrap()),
+                40 => String::from(*words.get(&number).unwrap()),
+                50 => String::from(*words.get(&number).unwrap()),
+                60 => String::from(*words.get(&number).unwrap()),
+                70 => String::from(*words.get(&number).unwrap()),
+                80 => String::from(*words.get(&number).unwrap()),
+                90 => String::from(*words.get(&number).unwrap()),
+                21..=99 => String::from_iter([
+                    get_hundred((number / 10) * 10, words),
+                    " ".to_string(),
+                    get_hundred(number % 10, words),
+                ]),
+                100..=999 => {
+                    let mut res = String::from_iter([
+                        get_hundred(number / 100, words),
+                        " Hundred".to_string(),
+                    ]);
+                    if number % 100 > 0 {
+                        res.extend([" ", get_hundred(number % 100, words).as_str()]);
+                    }
+                    res
+                }
+                _ => panic!("this function can't be used in numbers greater than 3 digits"),
+            }
+        }
+        let mut res = String::from("");
+        //convert billion
+        let left = num / 1000000000;
+        num %= 1000000000;
+        let str_result = get_hundred(left, &words);
+        if str_result.len() > 0 {
+            res.push_str(str_result.as_str());
+            res.push_str(" Billion");
+        }
+        // println!("{left}, {str_result}, {res}");
+        //convert million
+        let left = num / 1000000;
+        num %= 1000000;
+        let str_result = get_hundred(left, &words);
+        if str_result.len() > 0 {
+            if res.len() > 0 {
+                res.push_str(" ");
+            }
+            res.push_str(str_result.as_str());
+            res.push_str(" Million");
+        }
+        // println!("{left}, {str_result}, {res}");
+        //convert thousands
+        let left = num / 1000;
+        num %= 1000;
+        let str_result = get_hundred(left, &words);
+        if str_result.len() > 0 {
+            if res.len() > 0 {
+                res.push_str(" ");
+            }
+            res.push_str(str_result.as_str());
+            res.push_str(" Thousand");
+        }
+        // println!("{left}, {str_result}, {res}");
+        //convert hundreds
+        let str_result = get_hundred(num, &words);
+        if str_result.len() > 0 {
+            if res.len() > 0 {
+                res.push_str(" ");
+            }
+            res.push_str(str_result.as_str());
+        }
+        // println!("{num}, {str_result}, {res}");
+
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        assert_eq!(
+            Solution::number_to_words(123),
+            String::from("One Hundred Twenty Three")
+        );
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(
+            Solution::number_to_words(12345),
+            String::from("Twelve Thousand Three Hundred Forty Five")
+        );
+    }
+    #[test]
+    fn test3() {
+        assert_eq!(
+            Solution::number_to_words(1234567891),
+            String::from("One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One")
+        );
+    }
+}
