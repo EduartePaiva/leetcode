679eb60ac3ad2159aa56935dccaa345ba2ee85b1
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-09 15:25:22 -0300

    840. Magic Squares In Grid

--- rust/src/daily/leetcode0840.rs
+++ rust/src/daily/leetcode0840.rs
@@ -13,39 +13,41 @@ impl Solution {
 
         for r in 0..rows - 2 {
             for c in 0..cols - 2 {
-                let f_r = grid[r][c] + grid[r + 1][c] + grid[r + 2][c];
-                let s_r = grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1];
-                if f_r != s_r {
-                    continue;
+                let mut visit = [0; 10];
+                let mut double = false;
+                for i in r..r + 3 {
+                    for j in c..c + 3 {
+                        match grid[i][j] {
+                            1..=9 => {
+                                visit[grid[i][j] as usize] += 1;
+                                if visit[grid[i][j] as usize] > 1 {
+                                    double = true;
+                                }
+                            }
+                            _ => {
+                                double = true;
+                                break;
+                            }
+                        }
+                    }
+                    if double {
+                        break;
+                    }
                 }
-                let t_r = grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2];
-                if f_r != t_r {
-                    continue;
+                if !double
+                    && grid[r][c] + grid[r + 1][c] + grid[r + 2][c] == 15
+                    && grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1] == 15
+                    && grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2] == 15
+                    && grid[r][c] + grid[r][c + 1] + grid[r][c + 2] == 15
+                    && grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2] == 15
+                    && grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2] == 15
+                    && grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] == 15
+                    && grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] == 15
+                {
+                    res += 1;
                 }
-                let f_c = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
-                if f_r != f_c {
-                    continue;
-                }
-                let s_c = grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2];
-                if f_r != s_c {
-                    continue;
-                }
-                let t_c = grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2];
-                if f_r != t_c {
-                    continue;
-                }
-                let d_1 = grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2];
-                if f_r != d_1 {
-                    continue;
-                }
-                let d_2 = grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c];
-                if f_r != d_2 {
-                    continue;
-                }
-                res += 1;
             }
         }
-
         res
     }
 }
@@ -62,6 +64,13 @@ mod tests {
                 vec![2, 7, 6, 2]
             ]),
             1
+        );
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(
+            Solution::num_magic_squares_inside(vec![vec![10, 3, 5], vec![1, 6, 11], vec![7, 9, 2]]),
+            0
         );
     }
 }
