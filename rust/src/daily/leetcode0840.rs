d966e98c028251c64d1e55d097312cf8771bd833
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-09 13:49:13 -0300

    half done

--- rust/src/daily/leetcode0840.rs
+++ rust/src/daily/leetcode0840.rs
@@ -1,7 +1,54 @@
 // 840. Magic Squares In Grid
 pub struct Solution;
 
-gi
+impl Solution {
+    pub fn num_magic_squares_inside(grid: Vec<Vec<i32>>) -> i32 {
+        if grid.len() < 3 || grid[0].len() < 3 {
+            return 0;
+        }
+        let rows = grid.len();
+        let cols = grid[0].len();
+        let mut res = 0;
+        //do the most brute force way
+
+        for r in 0..rows - 2 {
+            for c in 0..cols - 2 {
+                let f_r = grid[r][c] + grid[r + 1][c] + grid[r + 2][c];
+                let s_r = grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1];
+                if f_r != s_r {
+                    continue;
+                }
+                let t_r = grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2];
+                if f_r != t_r {
+                    continue;
+                }
+                let f_c = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
+                if f_r != f_c {
+                    continue;
+                }
+                let s_c = grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2];
+                if f_r != s_c {
+                    continue;
+                }
+                let t_c = grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2];
+                if f_r != t_c {
+                    continue;
+                }
+                let d_1 = grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2];
+                if f_r != d_1 {
+                    continue;
+                }
+                let d_2 = grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c];
+                if f_r != d_2 {
+                    continue;
+                }
+                res += 1;
+            }
+        }
+
+        res
+    }
+}
 
 #[cfg(test)]
 mod tests {
