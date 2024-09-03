63d8ba640348bdfc6edc3ae0e13a162877aab49a
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-09-03 09:34:30 -0300

    1945. Sum of Digits of String After Convert better solution

--- rust/src/daily/leetcode1945.rs
+++ rust/src/daily/leetcode1945.rs
@@ -3,24 +3,22 @@ pub struct Solution;
 
 impl Solution {
     pub fn get_lucky(s: String, k: i32) -> i32 {
-        let mut stack: Vec<i32> = vec![];
-
-        fn separate_digits(mut digit: i32) -> Vec<i32> {
-            let mut res = vec![];
-            while digit > 0 {
-                res.push(digit % 10);
-                digit /= 10;
+        fn compact_number(mut num: i32) -> i32 {
+            let mut r = 0;
+            while num > 0 {
+                r += num % 10;
+                num /= 10;
             }
-            res
+            r
         }
+        let mut res = 0;
         for b in s.as_bytes() {
-            stack.extend(separate_digits((b - b'a') as i32 + 1));
+            res += compact_number((b - b'a') as i32 + 1);
         }
         for _ in 1..k {
-            let sum: i32 = stack.iter().sum();
-            stack = separate_digits(sum);
+            res = compact_number(res);
         }
-        stack.into_iter().sum()
+        res
     }
 }
 
