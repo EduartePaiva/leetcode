8def1f4dcca903c8484fb6e39777d6f3c0f394b3
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-10-31 13:38:24 -0300

    2463. Minimum Total Distance Traveled

--- rust/src/daily/leetcode2463.rs
+++ rust/src/daily/leetcode2463.rs
@@ -5,42 +5,28 @@ impl Solution {
     pub fn minimum_total_distance(mut robot: Vec<i32>, mut factory: Vec<Vec<i32>>) -> i64 {
         robot.sort_unstable();
         factory.sort_unstable_by_key(|k| k[0]);
-        let mut unpacked_factory = vec![];
+        let mut factory_positions = vec![];
         for f in factory {
             for _ in 0..f[1] {
-                unpacked_factory.push(f[0]);
+                factory_positions.push(f[0]);
             }
         }
-        let mut cache = vec![vec![-1_i64; unpacked_factory.len()]; robot.len()];
-        fn dfs(
-            robot: &Vec<i32>,
-            unpacked_factory: &Vec<i32>,
-            rbt_i: usize,
-            fct_i: usize,
-            cache: &mut Vec<Vec<i64>>,
-        ) -> i64 {
-            if rbt_i == robot.len() {
-                return 0;
-            }
-            if fct_i == unpacked_factory.len() {
-                return -1;
-            }
-            if cache[rbt_i][fct_i] != -1 {
-                return cache[rbt_i][fct_i];
-            }
-            let opt_1 = dfs(robot, unpacked_factory, rbt_i, fct_i + 1, cache);
-            let opt_2 = unpacked_factory[fct_i].abs_diff(robot[rbt_i]) as i64
-                + dfs(robot, unpacked_factory, rbt_i + 1, fct_i + 1, cache);
-            let res = if opt_1.min(opt_2) != -1 {
-                opt_1.min(opt_2)
-            } else {
-                opt_1.max(opt_2)
-            };
-            cache[rbt_i][fct_i] = res;
-            res
+        let robot_count = robot.len();
+        let factory_count = factory_positions.len();
+
+        let mut dp = vec![vec![0_i64; factory_count + 1]; robot_count + 1];
+        for i in 0..robot.len() {
+            dp[i][factory_positions.len()] = 1e12 as i64;
         }
 
-        dfs(&robot, &unpacked_factory, 0, 0, &mut cache)
+        for r in (0..robot_count).rev() {
+            for c in (0..factory_count).rev() {
+                let assign = robot[r].abs_diff(factory_positions[c]) as i64 + dp[r + 1][c + 1];
+                let skip = dp[r][c + 1];
+                dp[r][c] = assign.min(skip);
+            }
+        }
+        dp[0][0]
     }
 }
 
